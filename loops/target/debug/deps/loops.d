41b17d7175f30af5f75424639bac13c5a066518a
Atazard <malmartius@gmail.com>
2023-07-13 19:44:59 +0300

    testing .gitignore

--- loops/target/debug/deps/loops.d
+++
@@ -1,5 +0,0 @@
-C:\Users\malma\Desktop\projects\rust\the_rust_programming_language\loops\target\debug\deps\loops.exe: src\main.rs
-
-C:\Users\malma\Desktop\projects\rust\the_rust_programming_language\loops\target\debug\deps\loops.d: src\main.rs
-
-src\main.rs:
--- loops/target/debug/loops.d
+++
@@ -1,1 +0,0 @@
-C:\Users\malma\Desktop\projects\rust\the_rust_programming_language\loops\target\debug\loops.exe: C:\Users\malma\Desktop\projects\rust\the_rust_programming_language\loops\src\main.rs
